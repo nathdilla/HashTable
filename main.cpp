13b2f38dc0bee227a9084dedbb83a5df9e39ad2a
Nathan Dilla <nathcdilla@protonmail.com>
2022-03-25 10:45:25 -0700

    string

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <algorithm>
 using namespace std;
 
@@ -11,25 +12,60 @@ struct node
 class HashTable
 {
 private:
-    const int SIZE = 8;
-    const int HASH = 8;
-    node *buckets[8] = {};
+    const int SIZE = 53;
+    const int HASH = 53;
+    node *buckets[53] = {};
 
 public:
     HashTable(){};
-    void insert(int data)
+    int hash(string str)
+    {
+        int data = 0;
+        for (int i = 0; i < str.length(); i++)
+        {
+            data = data + str[i];
+        }
+        return data;
+    };
+    void insert(string str)
     {
         node *newNode = new node;
+        int data = hash(str);
+        // cout << data << endl;
         newNode->data = data;
         newNode->next = buckets[data % HASH];
         buckets[data % HASH] = newNode;
     };
-    void search(){};
+    bool find(string str)
+    {
+        node *temp;
+        bool res = false;
+        int data = hash(str);
+        int inspt = 0;
+        for (int i = 0; i < SIZE; i++)
+        {
+            int x = i % HASH;
+            temp = buckets[x];
+            while (temp != NULL)
+            {
+                inspt++;
+                if (temp->data == data)
+                {
+                    res = true;
+                }
+                temp = temp->next;
+            }
+        }
+        cout << "inspected " + to_string(inspt) + " elements.." << endl;
+        ;
+        return res;
+    };
     void display()
     {
         node *temp;
         for (int i = 0; i < SIZE; i++)
         {
+            cout << "#";
             int x = i % HASH;
             temp = buckets[x];
             while (temp != NULL)
@@ -47,17 +83,37 @@ int main()
     srand(time(0));
     cout << "nathan's hash table" << endl;
     HashTable myHashTable;
-    myHashTable.insert(3);
-    myHashTable.insert(8);
-    myHashTable.insert(1);
-    myHashTable.insert(10);
-    myHashTable.insert(15);
-    myHashTable.insert(18);
-    myHashTable.insert(21);
-    myHashTable.insert(42);
-    myHashTable.insert(64);
-    myHashTable.insert(12);
-    myHashTable.insert(11);
+
+    ifstream fin("input.txt"); // read from file
+    string txt;
+
+    if (!fin)
+    {
+        cout << "File not found\n";
+        exit(2);
+    }
+
+    while (getline(fin, txt))
+    {
+        cout << "inserting " + txt << endl;
+        myHashTable.insert(txt);
+    }
     myHashTable.display();
+    cout << "******************" << endl;
+    string input = "";
+    while (input != "-1")
+    {
+        cout << "enter a word to search for:" << endl;
+        cin >> input;
+        if (myHashTable.find(input))
+        {
+            cout << "found " + input << endl;
+        }
+        else
+        {
+            cout << "not found" << endl;
+        }
+    }
+    cout << "Goodbye! :]" << endl;
     return 0;
 }
